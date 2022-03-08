115d7afd646add7ce0c0ff4825f64670ed76f7ce
suresh <suresh1502067@gmail.com>
2022-03-08 17:17:24 +0530

    sf

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>
         Counter program using context
       </h1>
+      <pre>I wanna coding</pre>
       <div style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
       <button style={{marginRight:20}}>
          sub counter
